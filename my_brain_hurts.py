846b879a1cefc314c80996d97e533440f9ce2037
Richard Wossal <richard@r-wos.org>
2011-09-27 00:02:06 +0200

    ...and that would bew 294 bytes, my bad

--- my_brain_hurts.py
+++ my_brain_hurts.py
@@ -3,7 +3,7 @@ o=[]
 for c in read(0,9**9):
  o.append({'>':'p+=1','<':'p-=1','+':'s[p]+=1','-':'s[p]-=1','.':
  'write(1,chr(s[p]))',',':'s[p]=ord(read(0,1))','[':
- 'while s[p]!=0:',']':0}.get(c,' '))
+ 'while s[p]!=0:',']':0}.get(c,''))
 r='s=[0]*8**5'
 p=0
 for e in o:
